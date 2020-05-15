706f505d8bb90b5e0819d4bbfea80923801b316a
samee <samee@192.168.120.23>
2020-05-15 23:46:37 +0900

    포기...

---
+++ Semi_Project/src/artistapply/model/service/ApplyService.java
@@ -0,0 +1,31 @@
+package artistapply.model.service;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import artistapply.model.dao.ApplyDao;
+import artistapply.model.vo.Apply;
+import artistapply.model.vo.Career;
+
+import static common.JDBCTemplate.*;
+
+public class ApplyService {
+
+	public int insertApply(Apply apply, ArrayList<Career> carlist) {
+		Connection conn = getConnection();
+		
+		ApplyDao ad = new ApplyDao();
+		
+		int result1 = ad.insertApply(conn, apply);
+		int result2 = ad.insertCareer(conn, carlist);
+		
+		if(result1 > 0 && result2 > 0) {
+			commit(conn);
+		} else {
+			rollback(conn);
+		}
+		
+		return result2;
+	}
+
+}
